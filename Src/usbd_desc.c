03ea9a2b7aafb8bc9dc801316b57c0cf4b38314d
Raoul Rubien <rubienr@sbox.tugraz.at>
2023-11-17 16:45:12 +0100

    updates USB VID and PID

--- Src/usbd_desc.c
+++ Src/usbd_desc.c
@@ -62,11 +62,11 @@
   * @{
   */
 
-#define USBD_VID     1155
+#define USBD_VID     4617
 #define USBD_LANGID_STRING     1033
-#define USBD_MANUFACTURER_STRING     "STMicroelectronics"
-#define USBD_PID_FS     22336
-#define USBD_PRODUCT_STRING_FS     "STM32 Virtual ComPort"
+#define USBD_MANUFACTURER_STRING     "3DP Accelerometer"
+#define USBD_PID_FS     57626
+#define USBD_PRODUCT_STRING_FS     "3dpaxxel"
 #define USBD_CONFIGURATION_STRING_FS     "CDC Config"
 #define USBD_INTERFACE_STRING_FS     "CDC Interface"
 
